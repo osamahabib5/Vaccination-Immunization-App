dd03592ec8e0a22d3ce5d37f4aa7fb9d452c13bf
Shariq-90 <Shariqahmed80@hotmail.com>
2021-09-13 17:26:32 +0500

    made Changes to Login Page

--- src/app/screens/Login.js
+++ src/app/screens/Login.js
@@ -3,26 +3,28 @@ import { SafeAreaView, StyleSheet, Text, TextInput, View, Button, TouchableOpaci
 import { styles } from '../styles/style';
 import { Icon } from 'react-native-elements'
 import LinearGradient from 'react-native-linear-gradient';
-// import firebase from '../database/firebase';
+import {axios} from 'axios';
 
 export default function Login({ navigation, route }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [LoginDetails, setLoginDetails] = useState({email : "", password: ""})
   return (
     <View style={styles.container}>
       <LinearGradient style={styles.linearGradient} colors={['#e6f7fc', '#e6f7fc', '#0df2c9']}>
         <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
           <Image style={{
             marginTop: 5,
-            justifyContent: 'center', height: 115, width: 145, resizeMode: 'stretch', margin: 10
+            justifyContent: 'center', height: 120, width: 145, resizeMode: 'stretch', margin: 10
           }} source={require("../assets/logo.png")} />
         </View>
-        <View style={{ flex: 1, flexDirection: 'column', position: "relative", marginBottom: "0%", justifyContent: 'center' }}>
+        <View style={{ flex: 1, flexDirection: 'column', position: "relative", justifyContent: 'center',
+        marginTop: 80
+      }}>
           <View style={styles.inputView}>
             <TextInput
               style={styles.TextInput}
               placeholder="Email Address"
               placeholderTextColor="#00000087"
+              keyboardType='email-address'
             />
           </View>
           <View style={styles.inputView}>
@@ -34,7 +36,9 @@ export default function Login({ navigation, route }) {
             />
           </View>
         </View>
-        <View style={{ flex: 1, flexDirection: 'column', position: "relative", marginBottom: "0%", justifyContent: 'center' }}>
+        <View style={{ flex: 1, flexDirection: 'column', position: "relative", marginBottom: "0%", justifyContent: 'center',
+      marginTop: 50
+      }}>
           <TouchableOpacity
             onPress={() =>
               navigation.navigate('Home')
