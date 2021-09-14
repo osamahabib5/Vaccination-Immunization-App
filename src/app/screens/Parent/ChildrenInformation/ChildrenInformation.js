55683ec2473603bb7dc12b846efcc4deaf207be1
osamahabib5 <osama.habib@khi.iba.edu.pk>
2021-09-14 09:29:02 +0500

    Working on Parent Dashboard

--- src/app/screens/Parent/ChildrenInformation/ChildrenInformation.js
+++ src/app/screens/Parent/ChildrenInformation/ChildrenInformation.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { View , Text} from 'react-native'
+import { View , Text, TouchableOpacity} from 'react-native'
 
 function ChildrenInformation() {
     return (
-        <View>
-            <Text>
-                My name is Google
-            </Text>
+        <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+            <View style={{ flex: 1}}>
+                <Text>
+                    Child Name
+                </Text>
+            </View>
+            <View style = {{flex: 1}}>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('SignUp');
+                        ParentSignup();
+                    }} style={{ backgroundColor: '#1796b3', width: 65, height: 65, justifyContent: 'center', borderRadius: 40 }}>
+                    <Text style={{
+                        color: '#FFFFFF', textAlign: 'center', fontSize: 13, fontWeight: "500"
+                    }}>View Children</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
