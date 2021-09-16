df395cad38f7c2c5e9f98699d14fadd4bb6de0ee
Shariq-90 <Shariqahmed80@hotmail.com>
2021-09-16 20:43:02 +0500

    Deleted Stack Navigator

--- src/app/navigation/DrawerNavigator.js
+++ src/app/navigation/DrawerNavigator.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import EditProfileScreen from '../screens/Parent/EditProfileScreen';
-import { ParentStackNavigator, StackNavigator } from './StackNavigator';
 import ParentDashboard from '../screens/Parent/ParentDashboard';
 import ParentProfile from '../screens/Parent/ParentProfile';
 const Drawer = createDrawerNavigator();
