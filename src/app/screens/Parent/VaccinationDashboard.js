474b4cd4b9f777acf25c1fc3606ec77b2c93604a
Shariq-90 <Shariqahmed80@hotmail.com>
2021-09-26 22:05:18 +0500

    Completed Vaccination Details Page

--- src/app/screens/Parent/VaccinationDashboard.js
+++ src/app/screens/Parent/VaccinationDashboard.js
@@ -7,42 +7,53 @@ import {
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 // import Share from 'react-native-share';
 import { View, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native'
+import VaccinationDetails from './VaccinationDetails';
+import LinearGradient from 'react-native-linear-gradient';
+
 function VaccinationDashboard() {
     return (
         <SafeAreaView style={styles.container}>
-            <View>
-                <View style={{
-                    flexDirection: 'row'
-                }}>
-                    <View style = {{flex: 1,
-                    alignItems: 'center'
+            <LinearGradient style={styles.linearGradient}
+                colors={['#e6f7fc', '#e6f7fc', '#0df2c9']}>
+                <View>
+                    <View style={{
+                        flexDirection: 'row'
                     }}>
-                        <Title style={styles.title}>Vaccination Details
-                        </Title>
+                        <View style={{
+                            flex: 1,
+                            alignItems: 'center',
+                            top: 40
+                        }}>
+                            <Title style={styles.title}>Vaccination Details
+                            </Title>
+                        </View>
                     </View>
                 </View>
-            </View>
-            <View style={styles.userInfoSection}>
-                <View style={styles.row}>
-                    <Icon name="map-marker-radius" size={20} color="#777777" />
-                    <Text>Name</Text>
-                </View>
-                <View style={styles.row}>
-                    <Text>Manufacturer</Text>
-                </View>
-                <View style={styles.row}>
-                    <Icon name="phone" size={20} color="#777777" />
-                    <Text>Quantity</Text>
-                </View>
-                <View style={styles.row}>
-                    <Text>Expiry Date</Text>
-                </View>
-                <View style={styles.row, { marginTop: 20, alignSelf: 'center' }}>
-                    <TouchableOpacity >
-                        <Text style={styles.closeButton}>Hide</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+                {VaccinationDetails.map(data => {
+                    return (
+                        <View key={data.id} style={styles.userInfoSection}>
+                            <View style={styles.row}>
+                                <Text>
+                                    <Text style={[styles.infoStyling, { fontWeight: "bold" }]}> Name: </Text>
+                                    <Text style={styles.infoStyling}> {data.name}</Text>
+                                </Text>
+                            </View>
+                            <View style={styles.row}>
+                                <Text style={[styles.infoStyling, { fontWeight: "bold" }]}> Manufacturer: </Text>
+                                <Text style={styles.infoStyling}> {data.manufacturer}</Text>
+                            </View>
+                            <View style={styles.row}>
+                                <Text style={[styles.infoStyling, { fontWeight: "bold" }]}> Quantity:  </Text>
+                                <Text style={styles.infoStyling}> {data.quantity}</Text>
+                            </View>
+                            <View style={styles.row}>
+                                <Text style={[styles.infoStyling, { fontWeight: "bold" }]}> Country: </Text>
+                                <Text style={styles.infoStyling}> {data.country}</Text>
+                            </View>
+                        </View>
+                    )
+                })}
+            </LinearGradient>
         </SafeAreaView>
     )
 }
@@ -53,11 +64,11 @@ export default VaccinationDashboard
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        top: 30,
     },
     userInfoSection: {
-        paddingHorizontal: 30,
-        flex: 1,
+        paddingHorizontal: 10,
+        flex: 0.3,
+        marginTop: 80
     },
     title: {
         fontSize: 24,
@@ -72,36 +83,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         marginBottom: 10,
     },
-    infoBoxWrapper: {
-        borderBottomColor: '#dddddd',
-        borderBottomWidth: 1,
-        borderTopColor: '#dddddd',
-        borderTopWidth: 1,
-        flexDirection: 'row',
-        height: 100,
-    },
-    infoBox: {
-        width: '50%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    menuWrapper: {
-        marginTop: 10,
-    },
-    menuItem: {
-        flexDirection: 'row',
-        paddingVertical: 15,
-        paddingHorizontal: 30,
-    },
-    menuItemText: {
-        color: '#777777',
-        marginLeft: 20,
-        fontWeight: '600',
-        fontSize: 16,
-        lineHeight: 26,
-    },
     closeButton: {
         fontSize: 20,
         fontWeight: 'bold'
-    }
+    },
+    infoStyling: {
+        fontSize: 18
+    },
+    linearGradient: {
+        flex: 1,
+        width: '100%',
+
+    },
 })
